667fd0539b37c83b6280791358e0d7d066d7aae4
Yeabsira Fikre <fikreyabsira@gmail.com>
2026-04-14 16:26:27 +0300

    chore: remove test scripts with exposed anon key

--- test.js
+++
@@ -1,6 +0,0 @@
-const url = 'https://risvfdlhmnwvkngkllgn.supabase.co/functions/v1/tmdb?action=popular&type=movie';
-const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpc3ZmZGxobW53dmtuZ2tsbGduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5NDc2OTEsImV4cCI6MjA4OTUyMzY5MX0.0ZAk-tdybtxHqvTkcbfVgoG18hzPupXGGT03GUbnssM';
-
-fetch(url, { headers: { Authorization: `Bearer ${key}`, apikey: key }})
-  .then(res => res.text().then(text => console.log(res.status, text.substring(0, 500))))
-  .catch(err => console.error(err));
